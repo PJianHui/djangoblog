ad493e5584619b6ac12ae6dbf930eacb3403ac4a
PJianHui <pjh67890@sohu.com>
2017-09-21 10:04:19 +0800

    first commit

---
+++ article/views.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+# Create your views here.
+from article.models import Article
+from datetime import datetime
+
+# Create your views here.
+def home(request):
+    post_list = Article.objects.all()  #获取全部的Article对象
+    return render(request, 'home.html', {'post_list' : post_list})
+
+def detail(request, id):
+    try:
+        post = Article.objects.get(id=str(id))
+    except Article.DoesNotExist:
+        raise Http404
+    return render(request, 'post.html', {'post' : post})
+
+def archives(request) :
+    try:
+        post_list = Article.objects.all()
+    except Article.DoesNotExist :
+        raise Http404
+    return render(request, 'archives.html', {'post_list' : post_list, 
+                                            'error' : False})
+def about_me(request) :
+    return render(request, 'aboutme.html')
+
+def search_tag(request, tag) :
+    try:
+        post_list = Article.objects.filter(category = tag) #contains
+    except Article.DoesNotExist :
+        raise Http404
+    return render(request, 'tag.html', {'post_list' : post_list})
